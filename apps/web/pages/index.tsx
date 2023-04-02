035257a970bc2a3d9f59ef5eca402ac6955038fb
ymc182 <ymc182@gmail.com>
2023-04-03 00:27:04 +1000

    near-mono

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,24 @@
 import { Button } from "ui";
-
+import { NearProvider, useNearContext } from "near-context";
 export default function Web() {
-  return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
-  );
+	return (
+		<div>
+			<NearProvider>
+				<h1>Web</h1>
+				<TestAccountId />
+
+				<Button />
+			</NearProvider>
+		</div>
+	);
+}
+
+function TestAccountId() {
+	const { accountId, modal } = useNearContext();
+	return (
+		<div>
+			{accountId ? accountId : "Not Logged In"}
+			<button onClick={() => modal?.show()}>Login</button>
+		</div>
+	);
 }
